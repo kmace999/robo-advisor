ef48b51dce0db7f4c460afe14616b60d5b3123b8
kmace999 <kmace999@gmail.com>
2021-03-04 15:38:47 -0500

    data validation for symbol inputs in program

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # robo-advisor
+
+#Customizing your API key or something 
+
+Create a new file called ".env"
+
+> .env
+
+The required package for this game python-dotenv allows the user to enter a secret username that is displayed in the game but not visible when looking through the root depository. To change your username, open the new .env file in your text editor (example below),
+
+atom .env
+
+and enter your chosen username.
+
+USER_NAME = "Remy Ratatouille"
--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -14,3 +14,31 @@ print("RECOMMENDATION REASON: TODO")
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
+
+
+
+searchtickers = []
+entering = True
+
+while entering:
+    ticker = input("Please enter the ticker symbol of one stock or cryptocurrency (or enter 'finished' if done):")
+
+    if ticker == 'finished':
+        entering = False
+
+    nonumbers=True
+    tickerlist = list(ticker)
+    for i in tickerlist:
+        if i == "0" or i =="1" or i == "2" or i == "3" or i == "4" or i == "5" or i == "6" or i == "7" or i == "8" or i == "9":
+            print("Please enter a correctly formatted symbol like 'MSFT' (between 1 to 5 digits, does not include numeric digits).")
+            print("Please try again. ")
+            nonumbers = False
+            break
+
+    if nonumbers:
+        if (len(ticker) > 5 or len(ticker) == 0) and ticker != 'finished':
+            print("Please enter a correctly formatted symbol like 'MSFT' (between 1 to 5 digits).")
+            print("Please try again.")
+
+        elif len(ticker) > 0 and len(ticker) <= 5:
+            searchtickers.append(ticker)
