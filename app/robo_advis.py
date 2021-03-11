a50e3f123ac6b49cbd55d584937ed855d9ce1917
kmace999 <kmace999@gmail.com>
2021-03-11 17:38:44 -0500

    created recommendation algorithm

--- app/robo_advis.py
+++ app/robo_advis.py
@@ -2,7 +2,7 @@
 addmore = True
 entering = True
 
-import seaborn as sns
+#import seaborn as sns
 
 #start copied function from past project readme setups
 def to_usd(my_price):
@@ -105,20 +105,66 @@ while addmore:
     requestdate = thisday.strftime("%m/%d/%Y %H:%M")
 
 
+    #algorithm for recommendation
+    explanation = []
+    latestclose = dfrecords["close"].values
+    closed = latestclose[0]
+    recenthigh = dfrecords["high"].max()
+    recentlow = dfrecords["low"].min()
+    meanclose = dfrecords["close"].mean()
+    medclose = dfrecords["close"].median()
+    compratio = meanclose/medclose
+    #if the stock's latest closing price is less than 20% above its recent low buy, else don't buy (using the recommended method from the README)
+    compare1 = 1.2*recentlow
+    if closed < compare1:
+        rec1 = 1
+        explanation.append("The stock or cryptocurrency's latest closing price is less than 20% above its recent low.")
+    else:
+        rec1 = 0
+        explanation.append("The stock or cryptocurrency's latest closing price is greater than 20% above its recent low.")
+    #if the stock's latest closing price is more than 30% below it's recent high, don't buy, otherwise buy
+    compare2 = .7*recenthigh
+    if closed < compare2:
+        rec2 = 0
+        explanation.append("The stock or cryptocurrency's latest closing price is less than 30% below its recent high.")
+    else:
+        rec2 = 1
+        explanation.append("The stock or cryptocurrency's latest closing price is greater than 30% below its recent high.")
+
+    #compare median and mean to search for outliers
+    #if mean/median > 1.1 or mean/median < .9
+    if .9 < compratio < 1.1:
+        rec3 = 1
+        explanation.append("The stock or cryptocurrency's mean to median ratio is between 90% and 110%.")
+    else:
+        rec3=0
+        if compratio > 1.1:
+            explanation.append("The stock or cryptocurrency's mean to median ratio is greater than 110%.")
+        if compratio < .9:
+            explanation.append("The stock or cryptocurrency's mean to median ratio is less than 90%.")
+
+    if (rec3+rec2+rec1)>1:
+        recommendation = "BUY!"
+    else:
+        recommendation = "DON'T BUY"
+
+
+
     print("-------------------------")
     print(f"ADVICE REQUESTED AT: {requestdate}")
     print("-------------------------")
     latestdata = dfrecords["date"].values
     print(f"LATEST DATA FROM: {latestdata[0]}")
-    latestclose = dfrecords["close"].values
+
     print(f"LATEST CLOSE: {to_usd(latestclose[0])}")
-    recenthigh = dfrecords["high"].max()
+
     print(f"RECENT HIGH: {to_usd(recenthigh)}")
-    recentlow = dfrecords["low"].min()
     print(f"RECENT LOW: {to_usd(recentlow)}")
     print("-------------------------")
-    print("RECOMMENDATION: ")
+    print(f"RECOMMENDATION: {recommendation}")
     print("EXPLANATION: ")
+    for reason in explanation:
+        print(reason)
     print("-------------------------")
     print("HAPPY INVESTING :)")
     print("")
