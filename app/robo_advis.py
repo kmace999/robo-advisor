dc930eb4ef6c79b18eafb35bb9dcd61b9a9d068e
kmace999 <kmace999@gmail.com>
2021-03-11 17:46:44 -0500

    reimplemented try/except

--- app/robo_advis.py
+++ app/robo_advis.py
@@ -72,124 +72,124 @@ while addmore:
 
     request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&apikey={API_KEY}"
 
-    #try:
+    try:
 
-    response = requests.get(request_url)
-    parsed_response = json.loads(response.text)
+        response = requests.get(request_url)
+        parsed_response = json.loads(response.text)
 
-    for date, daily_data in parsed_response["Time Series (Daily)"].items():
+        for date, daily_data in parsed_response["Time Series (Daily)"].items():
 
-        record = {
-            "date": date,
-            "open": float(daily_data["1. open"]),
-            "high": float(daily_data["2. high"]),
-            "low": float(daily_data["3. low"]),
-            "close": float(daily_data["4. close"]),
-            "volume": int(daily_data["5. volume"]),
-        }
-        records.append(record)
+            record = {
+                "date": date,
+                "open": float(daily_data["1. open"]),
+                "high": float(daily_data["2. high"]),
+                "low": float(daily_data["3. low"]),
+                "close": float(daily_data["4. close"]),
+                "volume": int(daily_data["5. volume"]),
+            }
+            records.append(record)
 
-    #END CODE FROM CLASS COLAB
+        #END CODE FROM CLASS COLAB
 
-    import pandas as pd
-    from pandas import DataFrame
+        import pandas as pd
+        from pandas import DataFrame
 
-    dfrecords = DataFrame(records)
-    #print(dfrecords)
+        dfrecords = DataFrame(records)
+        #print(dfrecords)
 
-    dfrecords.to_csv('data/prices.csv', index = False)
-
-
-    from datetime import datetime
-    thisday = datetime.now()
-    requestdate = thisday.strftime("%m/%d/%Y %H:%M")
+        dfrecords.to_csv('data/prices.csv', index = False)
 
 
-    #algorithm for recommendation
-    explanation = []
-    latestclose = dfrecords["close"].values
-    closed = latestclose[0]
-    recenthigh = dfrecords["high"].max()
-    recentlow = dfrecords["low"].min()
-    meanclose = dfrecords["close"].mean()
-    medclose = dfrecords["close"].median()
-    compratio = meanclose/medclose
-    #if the stock's latest closing price is less than 20% above its recent low buy, else don't buy (using the recommended method from the README)
-    compare1 = 1.2*recentlow
-    if closed < compare1:
-        rec1 = 1
-        explanation.append("The stock or cryptocurrency's latest closing price is less than 20% above its recent low.")
-    else:
-        rec1 = 0
-        explanation.append("The stock or cryptocurrency's latest closing price is greater than 20% above its recent low.")
-    #if the stock's latest closing price is more than 30% below it's recent high, don't buy, otherwise buy
-    compare2 = .7*recenthigh
-    if closed < compare2:
-        rec2 = 0
-        explanation.append("The stock or cryptocurrency's latest closing price is less than 30% below its recent high.")
-    else:
-        rec2 = 1
-        explanation.append("The stock or cryptocurrency's latest closing price is greater than 30% below its recent high.")
-
-    #compare median and mean to search for outliers
-    #if mean/median > 1.1 or mean/median < .9
-    if .9 < compratio < 1.1:
-        rec3 = 1
-        explanation.append("The stock or cryptocurrency's mean to median ratio is between 90% and 110%.")
-    else:
-        rec3=0
-        if compratio > 1.1:
-            explanation.append("The stock or cryptocurrency's mean to median ratio is greater than 110%.")
-        if compratio < .9:
-            explanation.append("The stock or cryptocurrency's mean to median ratio is less than 90%.")
-
-    if (rec3+rec2+rec1)>1:
-        recommendation = "BUY!"
-    else:
-        recommendation = "DON'T BUY"
+        from datetime import datetime
+        thisday = datetime.now()
+        requestdate = thisday.strftime("%m/%d/%Y %H:%M")
 
 
+        #algorithm for recommendation
+        explanation = []
+        latestclose = dfrecords["close"].values
+        closed = latestclose[0]
+        recenthigh = dfrecords["high"].max()
+        recentlow = dfrecords["low"].min()
+        meanclose = dfrecords["close"].mean()
+        medclose = dfrecords["close"].median()
+        compratio = meanclose/medclose
+        #if the stock's latest closing price is less than 20% above its recent low buy, else don't buy (using the recommended method from the README)
+        compare1 = 1.2*recentlow
+        if closed < compare1:
+            rec1 = 1
+            explanation.append("The stock or cryptocurrency's latest closing price is less than 20% above its recent low.")
+        else:
+            rec1 = 0
+            explanation.append("The stock or cryptocurrency's latest closing price is greater than 20% above its recent low.")
+        #if the stock's latest closing price is more than 30% below it's recent high, don't buy, otherwise buy
+        compare2 = .7*recenthigh
+        if closed < compare2:
+            rec2 = 0
+            explanation.append("The stock or cryptocurrency's latest closing price is less than 30% below its recent high.")
+        else:
+            rec2 = 1
+            explanation.append("The stock or cryptocurrency's latest closing price is greater than 30% below its recent high.")
 
-    print("-------------------------")
-    print(f"ADVICE REQUESTED AT: {requestdate}")
-    print("-------------------------")
-    latestdata = dfrecords["date"].values
-    print(f"LATEST DATA FROM: {latestdata[0]}")
+        #compare median and mean to search for outliers
+        #if mean/median > 1.1 or mean/median < .9
+        if .9 < compratio < 1.1:
+            rec3 = 1
+            explanation.append("The stock or cryptocurrency's mean to median ratio is between 90% and 110%.")
+        else:
+            rec3=0
+            if compratio > 1.1:
+                explanation.append("The stock or cryptocurrency's mean to median ratio is greater than 110%.")
+            if compratio < .9:
+                explanation.append("The stock or cryptocurrency's mean to median ratio is less than 90%.")
 
-    print(f"LATEST CLOSE: {to_usd(latestclose[0])}")
-
-    print(f"RECENT HIGH: {to_usd(recenthigh)}")
-    print(f"RECENT LOW: {to_usd(recentlow)}")
-    print("-------------------------")
-    print(f"RECOMMENDATION: {recommendation}")
-    print("EXPLANATION: ")
-    for reason in explanation:
-        print(reason)
-    print("-------------------------")
-    print("HAPPY INVESTING :)")
-    print("")
+        if (rec3+rec2+rec1)>1:
+            recommendation = "BUY!"
+        else:
+            recommendation = "DON'T BUY"
 
 
-    #sns.lineplot(data=dfrecords, x="date", y="close")
+
+        print("-------------------------")
+        print(f"ADVICE REQUESTED AT: {requestdate}")
+        print("-------------------------")
+        latestdata = dfrecords["date"].values
+        print(f"LATEST DATA FROM: {latestdata[0]}")
+
+        print(f"LATEST CLOSE: {to_usd(latestclose[0])}")
+
+        print(f"RECENT HIGH: {to_usd(recenthigh)}")
+        print(f"RECENT LOW: {to_usd(recentlow)}")
+        print("-------------------------")
+        print(f"RECOMMENDATION: {recommendation}")
+        print("EXPLANATION: ")
+        for reason in explanation:
+            print(reason)
+        print("-------------------------")
+        print("HAPPY INVESTING :)")
+        print("")
 
 
-    moredatavalidation = True
-    while moredatavalidation:
-        morestocks = input("Do you wish to receive advice on another stock or cryptocurrency? ['yes'/'no']:")
-        if morestocks == "no":
-            addmore = False
-            moredatavalidation = False
-            print("Thank you for using the Robo Advisor!")
-        elif morestocks == "yes":
-            entering = True
-            moredatavalidation = False
-        elif morestocks != "yes" and morestocks !="no":
-            print("Please enter either 'yes' or 'no'.")
+        #sns.lineplot(data=dfrecords, x="date", y="close")
+
+
+        moredatavalidation = True
+        while moredatavalidation:
+            morestocks = input("Do you wish to receive advice on another stock or cryptocurrency? ['yes'/'no']:")
+            if morestocks == "no":
+                addmore = False
+                moredatavalidation = False
+                print("Thank you for using the Robo Advisor!")
+            elif morestocks == "yes":
+                entering = True
+                moredatavalidation = False
+            elif morestocks != "yes" and morestocks !="no":
+                print("Please enter either 'yes' or 'no'.")
 
 
 
 
 #UNCOMMENT THESE BEFORE YOU TURN IN!!!!!!!
-    #except:
-    #    print("Oops, that stock symbol was not found. Please try again.")
-    #    entering = True
+    except:
+        print("Oops, that stock symbol was not found. Please try again.")
+        entering = True
